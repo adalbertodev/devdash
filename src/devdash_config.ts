16c9459f0b09aa2929cb2a8bb7f13f0b57a026be
Adalberto <adalberto.devs@gmail.com>
2023-01-02 15:03:01 +0000

    config file addded

---
+++ src/devdash_config.ts
@@ -0,0 +1,25 @@
+export interface DevDashConfig {
+	github_access_token: string;
+	widgets: {
+		id: string;
+		repository_url: string;
+	}[];
+}
+
+export const config: DevDashConfig = {
+	github_access_token: process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN as string,
+	widgets: [
+		{
+			id: "fce4975a-8466-4add-bbcf-90027e3e8133",
+			repository_url: "https://github.com/CodelyTV/cra-template-codely",
+		},
+		{
+			id: "2085c265-dba4-49c4-b1ce-234d02923d34",
+			repository_url: "https://github.com/CodelyTV/eslint-config-codely",
+		},
+		{
+			id: "5607c9a8-e2f3-44f0-bf24-9c833b05068e",
+			repository_url: "https://github.com/CodelyTV/typescript-ddd-example",
+		},
+	],
+};
