ffc505ee2c7ab6bf8140fb0371076631ae9f7d0c
Adalberto <adalberto.devs@gmail.com>
2023-01-04 19:39:09 +0000

    widget component added

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -3,34 +3,8 @@ import { FC, useEffect, useState } from "react";
 import { config } from "../../devdash_config";
 import { GitHubRepository, GithubRepositoryRepository } from "../../domain";
 import styles from "./Dashboard.module.scss";
-import {
-	Brand,
-	Check,
-	Error,
-	Forks,
-	IssueOpened,
-	Lock,
-	PullRequests,
-	Start,
-	Unlock,
-	Watchers,
-} from "./svgs";
-
-const isoToReadableDate = (lastUpdateDate: Date): string => {
-	const currentDate = new Date();
-	const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
-	const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
-
-	if (diffDays === 0) {
-		return "today";
-	}
-
-	if (diffDays > 30) {
-		return "more than a month ago";
-	}
-
-	return `${diffDays} days ago`;
-};
+import { GitHubRepositoryWidget } from "./GitHubRepositoryWidget";
+import { Brand } from "./svgs";
 
 const title = "DevDash";
 
@@ -68,55 +42,10 @@ export const Dashboard: FC<Props> = ({ repository }) => {
 			) : (
 				<section className={styles.container}>
 					{repositoryData.map((widget) => (
-						<article key={`${widget.id.organization}/${widget.id.name}`} className={styles.widget}>
-							<header className={styles.widget__header}>
-								<h2 className={styles.widget__title}>
-									<a
-										href={widget.url}
-										target="_blank"
-										title={`${widget.id.organization}/${widget.id.name}`}
-										rel="noreferrer"
-									>
-										{widget.id.organization}/{widget.id.name}
-									</a>
-								</h2>
-								{widget.private ? <Lock /> : <Unlock />}
-							</header>
-
-							<div className={styles.widget__body}>
-								<div className={styles.widget__status}>
-									<p>Last update {isoToReadableDate(widget.updatedAt)}</p>
-									{widget.hasWorkflows && (
-										<div>{widget.isLastWorkflowSuccess ? <Check /> : <Error />}</div>
-									)}
-								</div>
-
-								<p className={styles.widget__description}>{widget.description}</p>
-							</div>
-
-							<footer className={styles.widget__footer}>
-								<div className={styles.widget__stat}>
-									<Start />
-									<span>{widget.stars}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<Watchers />
-									<span>{widget.watchers}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<Forks />
-									<span>{widget.forks}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<IssueOpened />
-									<span>{widget.issues}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<PullRequests />
-									<span>{widget.pullRequests}</span>
-								</div>
-							</footer>
-						</article>
+						<GitHubRepositoryWidget
+							key={`${widget.id.organization}/${widget.id.name}`}
+							widget={widget}
+						/>
 					))}
 				</section>
 			)}
---
+++ src/sections/dashboard/GitHubRepositoryWidget.tsx
@@ -0,0 +1,87 @@
+import { FC } from "react";
+
+import { GitHubRepository } from "../../domain";
+import styles from "./GitHubRepositoryWidget.module.scss";
+import {
+	Check,
+	Error,
+	Forks,
+	IssueOpened,
+	Lock,
+	PullRequests,
+	Start,
+	Unlock,
+	Watchers,
+} from "./svgs";
+
+const isoToReadableDate = (lastUpdateDate: Date): string => {
+	const currentDate = new Date();
+	const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
+	const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
+
+	if (diffDays === 0) {
+		return "today";
+	}
+
+	if (diffDays > 30) {
+		return "more than a month ago";
+	}
+
+	return `${diffDays} days ago`;
+};
+
+interface Props {
+	widget: GitHubRepository;
+}
+
+export const GitHubRepositoryWidget: FC<Props> = ({ widget }) => {
+	return (
+		<article key={`${widget.id.organization}/${widget.id.name}`} className={styles.widget}>
+			<header className={styles.widget__header}>
+				<h2 className={styles.widget__title}>
+					<a
+						href={widget.url}
+						target="_blank"
+						title={`${widget.id.organization}/${widget.id.name}`}
+						rel="noreferrer"
+					>
+						{widget.id.organization}/{widget.id.name}
+					</a>
+				</h2>
+				{widget.private ? <Lock /> : <Unlock />}
+			</header>
+
+			<div className={styles.widget__body}>
+				<div className={styles.widget__status}>
+					<p>Last update {isoToReadableDate(widget.updatedAt)}</p>
+					{widget.hasWorkflows && <div>{widget.isLastWorkflowSuccess ? <Check /> : <Error />}</div>}
+				</div>
+
+				<p className={styles.widget__description}>{widget.description}</p>
+			</div>
+
+			<footer className={styles.widget__footer}>
+				<div className={styles.widget__stat}>
+					<Start />
+					<span>{widget.stars}</span>
+				</div>
+				<div className={styles.widget__stat}>
+					<Watchers />
+					<span>{widget.watchers}</span>
+				</div>
+				<div className={styles.widget__stat}>
+					<Forks />
+					<span>{widget.forks}</span>
+				</div>
+				<div className={styles.widget__stat}>
+					<IssueOpened />
+					<span>{widget.issues}</span>
+				</div>
+				<div className={styles.widget__stat}>
+					<PullRequests />
+					<span>{widget.pullRequests}</span>
+				</div>
+			</footer>
+		</article>
+	);
+};
