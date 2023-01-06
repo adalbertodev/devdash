e8ad080b312628795e91cc05b9cc5898ce763747
Adalberto <adalberto.devs@gmail.com>
2023-01-06 11:10:54 +0000

    topbar progress added to layout

--- src/sections/detail/GitHubRepositoryDetail.tsx
+++ src/sections/detail/GitHubRepositoryDetail.tsx
@@ -1,5 +1,5 @@
 import { FC, useMemo } from "react";
-import { Link, Navigate, useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 import { Lock, Unlock } from "../../assets/svgs";
 import { GithubRepositoryRepository } from "../../domain";
@@ -27,11 +27,11 @@ export const GitHubRepositoryDetail: FC<Props> = ({ repository }) => {
 	return (
 		<section className={styles["repository-detail"]}>
 			<header className={styles.header}>
-				<Link to={repositoryData.url} target="_blank" rel="noreferrer">
+				<a href={repositoryData.url} target="_blank" rel="noreferrer">
 					<h2 className={styles.header__title}>
 						{repositoryData.id.organization}/{repositoryData.id.name}
 					</h2>
-				</Link>
+				</a>
 				{repositoryData.private ? <Lock /> : <Unlock />}
 			</header>
 
--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -2,12 +2,15 @@ import { Link, Outlet } from "react-router-dom";
 
 import { ErrorBoundary } from "./ErrorBoundary";
 import styles from "./Layout.module.scss";
+import { TopBarProgressByLocation } from "./TopBarProgressByLocation";
 
 const title = "DevDash_";
 
 export const Layout = () => {
 	return (
 		<>
+			<TopBarProgressByLocation />
+
 			<header className={styles.header}>
 				<section className={styles.header__container}>
 					<Link to="/">
