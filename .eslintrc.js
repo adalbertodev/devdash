8567a4877e285d1f704c4000bc7438621f4b9318
Adalberto <adalberto.devs@gmail.com>
2023-01-27 16:26:07 +0000

    eslint config moved to its own file

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	extends: ["react-app", "react-app/jest", "eslint-config-codely/typescript"],
+	parserOptions: {
+		project: ["./tsconfig.json"],
+	},
+	settings: {
+		"import/resolver": {
+			node: {
+				extensions: [".js", ".jsx", ".ts", ".tsx"],
+			},
+		},
+	},
+	overrides: [
+		{
+			files: ["**/tests/e2e/**/*.spec.ts"],
+			rules: {
+				"testing-library/await-async-query": 0,
+				"@typescript-eslint/no-unsafe-member-access": 0,
+				"@typescript-eslint/no-unsafe-call": 0,
+				"testing-library/prefer-screen-queries": 0,
+			},
+		},
+	],
+	rules: {
+		"prettier/prettier": [
+			"error",
+			{
+				endOfLine: "auto",
+			},
+		],
+	},
+};
