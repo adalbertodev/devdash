f66e128b490bd32f74ed3b978f60e978a7e71d76
Adalberto <adalberto.devs@gmail.com>
2023-01-28 16:00:24 +0000

    lighthouse config with only performance

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,20 @@
 module.exports = {
-  ci: {
-    assert: {
-      preset: "lighthouse:no-pwa"
-    }
-  }
-}
+	ci: {
+		assert: {
+			preset: "lighthouse:no-pwa",
+			assertions: {
+				"tap-targets": "off",
+				"non-composited-animations": "off",
+				"button-name": "off",
+				"color-contrast": "off",
+				"cps-xss": "off",
+				"errors-in-console": "off",
+				"external-anchors-use-rel-noopener": "off",
+				"heading-order": "off",
+				"link-name": "off",
+				"meta-description": "off",
+				"uses-text-compression": "off",
+			},
+		},
+	},
+};
